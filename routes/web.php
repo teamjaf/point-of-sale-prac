772877c81e853da062850e1bd1f744a68ee21397
Md. Jafrin Hossain <md.jafrin.hossain@g.bracu.ac.bd>
2023-03-01 01:19:53 +0600

    change pass added

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,6 @@ require __DIR__.'/auth.php';
 Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
 Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+Route::get('/change/password', [AdminController::class, 'ChangePassword'])->name('change.password');
+Route::post('/update/password', [AdminController::class, 'UpdatePassword'])->name('update.password');
  
