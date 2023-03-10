bcd2a9f043363247888952e4d70b27dd97b65329
Md. Jafrin Hossain <md.jafrin.hossain@g.bracu.ac.bd>
2023-03-11 00:50:29 +0600

    pos ui part two

--- app/Http/Controllers/Backend/PosController.php
+++ app/Http/Controllers/Backend/PosController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Customer;
 
 
 
 class PosController extends Controller
 {
     public function Pos(){
-        $product = Product::latest()->get();
-        return view('backend.pos.pos_page',compact('product'));
+        $product = Product::latest()->get();        
+        $customer = Customer::latest()->get();
+        return view('backend.pos.pos_page',compact('product','customer'));
 
     } // End Method 
 
