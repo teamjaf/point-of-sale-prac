6918cd0dfee6004d9be656c7e03084be4370b085
Md. Jafrin Hossain <md.jafrin.hossain@g.bracu.ac.bd>
2023-03-11 12:16:20 +0600

    adding invoice system

--- routes/web.php
+++ routes/web.php
@@ -169,6 +169,9 @@ Route::controller(EmployeeController::class)->group(function(){
         Route::get('/allitem','AllItem');
         Route::post('/cart-update/{rowId}','CartUpdate');
         Route::get('/cart-remove/{rowId}','CartRemove');
+
+
+        Route::post('/create-invoice','CreateInvoice');
     });
 
 });
